3ce9b1db9c866405d752c493d8477ac3153ce685
Drew <rodandrew95@gmail.com>
2020-11-08 20:07:27 +0200

    Swap over to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_11_08_154546) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_11_08_154546) do
   end
 
   create_table "inventory_items", force: :cascade do |t|
-    t.integer "kitchen_item_id", null: false
+    t.bigint "kitchen_item_id", null: false
     t.integer "count"
     t.float "quantity"
     t.float "price"
@@ -51,10 +54,10 @@ ActiveRecord::Schema.define(version: 2020_11_08_154546) do
   end
 
   create_table "recipe_items", force: :cascade do |t|
-    t.integer "inventory_item_id", null: false
+    t.bigint "inventory_item_id", null: false
     t.float "quantity"
     t.string "quantity_scale"
-    t.integer "recipe_id", null: false
+    t.bigint "recipe_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["inventory_item_id"], name: "index_recipe_items_on_inventory_item_id"
@@ -69,7 +72,7 @@ ActiveRecord::Schema.define(version: 2020_11_08_154546) do
   end
 
   create_table "sales", force: :cascade do |t|
-    t.integer "recipe_id", null: false
+    t.bigint "recipe_id", null: false
     t.integer "discount_percentage"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
