045aaedf0b69ea2ab6d1581f379425d687fd5fba
Drew <rodandrew95@gmail.com>
2020-11-09 04:02:21 +0200

    Add cost validation to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ApplicationRecord
   has_one_attached :photo
   has_many :recipe_items
 
+  validates :cost, presence: true
+
   def total_cost
     total = 0
     recipe_items.each {|item| total += item.cost}
