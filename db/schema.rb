3b0efbda076d4c1bad404bb72aee25d69c1507bf
Drew <rodandrew95@gmail.com>
2020-11-13 22:26:33 -0800

    Change discount percentage to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_154546) do
+ActiveRecord::Schema.define(version: 2020_11_14_062459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2020_11_08_154546) do
 
   create_table "sales", force: :cascade do |t|
     t.bigint "recipe_id", null: false
-    t.integer "discount_percentage"
+    t.float "discount_percentage"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "receipt_number"
