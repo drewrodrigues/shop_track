8a4272d8c89570a5e19f1e9b46a71d9af77d5b63
Drew <rodandrew95@gmail.com>
2020-11-19 22:22:15 +0200

    Redirect to kitchen items index on create

--- app/controllers/kitchen_items_controller.rb
+++ app/controllers/kitchen_items_controller.rb
@@ -1,34 +1,24 @@
 class KitchenItemsController < ApplicationController
   before_action :set_kitchen_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /kitchen_items
-  # GET /kitchen_items.json
   def index
     @kitchen_items = KitchenItem.order("name asc").all
   end
 
-  # GET /kitchen_items/1
-  # GET /kitchen_items/1.json
-  def show
-  end
+  def show; end
 
-  # GET /kitchen_items/new
   def new
     @kitchen_item = KitchenItem.new
   end
 
-  # GET /kitchen_items/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /kitchen_items
-  # POST /kitchen_items.json
   def create
     @kitchen_item = KitchenItem.new(kitchen_item_params)
 
     respond_to do |format|
       if @kitchen_item.save
-        format.html { redirect_to @kitchen_item, notice: 'Kitchen item was successfully created.' }
+        format.html { redirect_to kitchen_items_path, notice: 'Kitchen item was successfully created.' }
         format.json { render :show, status: :created, location: @kitchen_item }
       else
         format.html { render :new }
@@ -37,8 +27,6 @@ class KitchenItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /kitchen_items/1
-  # PATCH/PUT /kitchen_items/1.json
   def update
     respond_to do |format|
       if @kitchen_item.update(kitchen_item_params)
@@ -51,8 +39,6 @@ class KitchenItemsController < ApplicationController
     end
   end
 
-  # DELETE /kitchen_items/1
-  # DELETE /kitchen_items/1.json
   def destroy
     @kitchen_item.destroy
     respond_to do |format|
@@ -62,13 +48,12 @@ class KitchenItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_kitchen_item
-      @kitchen_item = KitchenItem.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def kitchen_item_params
-      params.require(:kitchen_item).permit(:name)
-    end
+  def set_kitchen_item
+    @kitchen_item = KitchenItem.find(params[:id])
+  end
+
+  def kitchen_item_params
+    params.require(:kitchen_item).permit(:name)
+  end
 end
