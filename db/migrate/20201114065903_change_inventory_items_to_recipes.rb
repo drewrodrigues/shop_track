cd58882c948e48bba4900802b36b60ec34d7247e
Drew <rodandrew95@gmail.com>
2020-11-19 21:20:47 +0200

    Fix migrations

--- db/migrate/20201114065903_change_inventory_items_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class ChangeInventoryItemsToRecipes < ActiveRecord::Migration[6.0]
-  def change
-    rename_table :receipts, :receipts
-  end
-end
--- db/migrate/20201114073411_change.rb
+++
@@ -1,5 +0,0 @@
-class Change < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :recipe_items, :inventory_item_id, :receipt_id
-  end
-end
