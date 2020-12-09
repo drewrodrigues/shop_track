09315028afa1459e481dbd4291114496ed17bb29
Drew <rodandrew95@gmail.com>
2020-12-09 02:58:20 +0200

    Terrible but working implementation of subitems with cost

--- app/models/recipe_combined_item.rb
+++ app/models/recipe_combined_item.rb
@@ -13,10 +13,27 @@ class RecipeCombinedItem < ApplicationRecord
   belongs_to :recipe
   belongs_to :combined
 
+  has_many :combined_items, through: :combined
+
   delegate :name, to: :combined
   delegate :quantity_scale, to: :combined
 
   def cost
     combined.cost_per_quantity * quantity
+  end
+
+  def items_with_costs(recursive_items = nil, recursive_item_quantity = nil)
+    items = []
+    (recursive_items || combined_items).each do |item|
+      if item.itemable_type == 'Combined'
+        items = items + items_with_costs(item.itemable.combined_items, item.quantity)
+      else
+        needed_quantity_ratio = (recursive_item_quantity || quantity) / combined.quantity
+        used_quantity = item.quantity * needed_quantity_ratio
+        item_cost = used_quantity * item.cost_per_measurement
+        items << {name: item.name, quantity: used_quantity.round(2), cost: item_cost.round(2)}
+      end
+    end
+    items
   end
 end
