344247417c8b70822d12a42c0fe8ef5361e9753b
Drew <rodandrew95@gmail.com>
2020-12-12 21:06:14 +0200

    Show discounts given

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,12 @@
 class DashboardController < ApplicationController
   def index
     @drink_with_total = SaleItem.drinks_with_total
+    profit_without_account_for_discounts = 0
     @net_profit = 0
     SaleItem.joins(:recipe).where.not(recipe_id: nil).each do |sale_item|
+      profit_without_account_for_discounts += sale_item.recipe.sale_price
       @net_profit += sale_item.recipe.profit_margin * sale_item.pos_sum
     end
+    @discounts_given = profit_without_account_for_discounts - @net_profit
   end
 end
