e43b98f2d9e95146646121cd85f557c117caa09d
Drew <rodandrew95@gmail.com>
2020-12-13 12:32:37 +0200

    Fix typo

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,16 +1,16 @@
 class DashboardController < ApplicationController
   def index
     @drink_with_total = SaleItem.drinks_with_total
-    profit_without_account_for_discounts = 0
+    profit_without_accounting_for_discounts = 0
     @net_profit = 0
 
     SaleItem.includes(:recipe, recipe: [:recipe_items, :recipe_combined_items]).where.not(recipe_id: nil).each do |sale_item|
-      profit_without_account_for_discounts += sale_item.recipe.sale_price
+      profit_without_accounting_for_discounts += sale_item.recipe.sale_price
       @net_profit += sale_item.pos_sum - sale_item.recipe.total_cost
     end
 
-    @discounts_given = profit_without_account_for_discounts - @net_profit
-    @gross_sales_removing_discounts = profit_without_account_for_discounts
+    @discounts_given = profit_without_accounting_for_discounts - @net_profit
+    @gross_sales_removing_discounts = profit_without_accounting_for_discounts
     @stats = {
       last_7_days: {
         sales_per_day: Sale.amount_in_last_days(7).to_i / 7,
